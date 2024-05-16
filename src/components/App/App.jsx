0367e3bc8d2ef5e7063088a5c5fe5d0125fa33b5
nataliia <sheludkonataliyavaleriivna@gmail.com>
2024-05-17 00:37:46 +0300

    add app component

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./App.css";
+import defaultEvents from "../../defaultEvents.json";
+
+export default function App() {
+	const [events, setEvents] = useState([]);
+
+
+
+  return <></>;
+}
