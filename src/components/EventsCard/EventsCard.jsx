426f25a647f5b4f24045f14f233f62165c09c283
nataliia <sheludkonataliyavaleriivna@gmail.com>
2024-05-17 18:32:55 +0300

    add eventsCard

--- src/components/EventsCard/EventsCard.jsx
+++ src/components/EventsCard/EventsCard.jsx
@@ -1,14 +1,28 @@
 import css from "../EventsCard/EventsCard.module.css";
 
-export default function Contact( {events: { title, description, date, organizer }}) {
+export default function Contact({
+  defaultEvents: { title, description, date, organizer },
+  onClick,
+  onBtn,
+}) {
   return (
     <div className={css.events}>
-			<h3>{ title}</h3>
-			<p>{ description}</p>
-			<p>{ date}</p>
-			<p>{organizer}</p>
-			<button>Register</button>
-			<button>View</button>
+      <div className={css.description}>
+        <h3 className={css.title}>{title}</h3>
+        <p>{description}</p>
+      </div>
+      <div className={css.info}>
+        <p>{new Date(date).toLocaleDateString()}</p>
+        <p>{organizer}</p>
+      </div>
+      <div className={css.links}>
+        <a href="#" onClick={onBtn} className={css.linkButton}>
+          Register
+        </a>
+        <a href="#" onClick={onClick} className={css.linkButton}>
+          View
+        </a>
+      </div>
     </div>
   );
 }
